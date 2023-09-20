574b8eb74145aad5411afe32e19a88b8b6cbb852
Fiosegura <fiorella.seguravargas@ucr.ac.cr>
2023-09-19 19:31:52 -0600

    Ejercicio 3 sin el gnuplot

--- laboratorio2.sh
+++ laboratorio2.sh
@@ -1,33 +1,23 @@
-#Laboratorio 2 del curso de plataformas acerca de procesos y servicios utilizando el branch de git. 
 #!/bin/bash
 
-#Ejercicio 1, el script recibe el ID de un proceso y devuelve información del mismo.
-echo -n "Escriba el número de ID del proceso:" && read ID 
-#Encontrar el nombre del proceso
-nombre=$(ps -p "$ID" -o comm=)
-echo "El nombre del proceso es: $nombre"
-#El ID del proceso
-echo "El numero del proceso es: $ID"
-#El parent process ID
-ppid=$(ps -p "$ID" -o ppid=)
-echo "El ppid es: $ppid"
-#Propietario
-user=$(ps -p "$ID" -o user=)
-echo "El usuario propietario es: $user"
-#Porcentaje de CPU
-cpu=$(ps -p "$ID" -o %cpu)
-echo "El porcentaje de cpu usado es: $cpu"
-#Consumo de memoria
-memoria=$(ps -p "$ID" -o %mem)
-echo "El consumo de memoria es de: $memoria"
-#Estado del proceso 
-stat=$(ps -p "$ID" -o stat)
-echo "El estado del proceso es: $stat"
-#El path del ejecutable
-path=$(readlink /proc/$ID/exe)
-echo "La ruta del ejecutable del proceso es: $path"
 
 #Ejercicio 3 recibir un ejecutable, correrlo y crear un archivo de log de consumo cpu y memoria, para graficarlo con gnuplot.
+echo -n "Escriba el ejecutable que desea:" && read ejecutable
+# Ejecuta el ejecutable binario
+$ejecutable &
+# Espera para que el proceso empiece
+sleep 2
+# Obtén el PID del proceso recién iniciado
+identificador=$!
+log="log_file.txt"
+echo "Tiempo | CPU (%) | Memoria (KB)" > "$log"
 
+while (ps -p "$identificador" > /dev/null); do
+	tiempo=$(date "+%M:%S")
+	CPU=$(ps -p "$identificador" -o %cpu)
+	MEMORIA=$(ps -p "$identificador" -o %mem)
+	echo ""$tiempo"  "$CPU"  "$MEMORIA"" >> "$log"
+	sleep 3
+done	
 
-
+cat "log_file.txt"
